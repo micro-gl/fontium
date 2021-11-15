dab159c37b839e1e938f992347d6dc918bf6dd7d
tshalev <tomer.shalev@gmail.com>
2021-11-15 12:55:40 +0200

    update license on some files

--- include/fontium/LayoutFactory.h
+++ include/fontium/LayoutFactory.h
@@ -2,6 +2,16 @@
 
 #include <fontium/layouters/boxlayouter.h>
 #include <fontium/layouters/boxlayouteroptimized.h>
+/*========================================================================================
+ Copyright (2021), Tomer Shalev (tomer.shalev@gmail.com, https://github.com/HendrixString).
+ All Rights Reserved.
+ License is a custom open source semi-permissive license with the following guidelines:
+ 1. unless otherwise stated, derivative work and usage of this file is permitted and
+    should be credited to the project and the author of this project.
+ 2. Unless required by applicable law or agreed to in writing, software
+    distributed under the License is distributed on an "AS IS" BASIS,
+    WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+========================================================================================*/
 #include <fontium/layouters/gridlayouter.h>
 #include <fontium/layouters/gridlinelayouter.h>
 #include <fontium/layouters/linelayouter.h>
